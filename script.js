7b9078e6a9b5abbae5048f4c97206c775c0d994b
ALOK KUMAR PANDEY <alokpandey2095@gmail.com>
2022-03-09 12:33:32 +0530

    facebook-pages

---
+++ script.js
@@ -0,0 +1,47 @@
+    var commentSectionEl=document.getElementById('commentSection');
+    let expandMenuEl=document.getElementById('expandMenu')
+    let seeMoreEl=document.getElementById('seeMore')
+    let addMenuEl=document.getElementById('addMenu')
+    let notificationMenuEl=document.getElementById('notificationMenu')
+    let messangeMenuEl=document.getElementById('messangeMenu')
+    function commentSection(){
+        commentSectionEl.classList.toggle('comment_section_display');
+    }
+
+
+    var shareEl=document.getElementById('share');
+
+    function share(){
+        shareEl.classList.toggle('share_display')
+    }
+    function expandMenu(){
+        expandMenuEl.classList.toggle('comment_section_display');
+    }
+    function expandMore(){
+        seeMoreEl.classList.toggle('comment_section_display');
+    }
+    function addMenu(){
+        addMenuEl.classList.toggle('comment_section_display')
+    }
+    function notificationMenu(){
+        notificationMenuEl.classList.toggle('comment_section_display')
+    }
+    function messangeMenu(){
+        messangeMenuEl.classList.toggle('comment_section_display')
+    }
+    var shareSecondEl=document.getElementById('shareSecond')
+    function shareSecond(){
+        shareSecondEl.classList.toggle('comment_section_display')
+    }
+    let commentSectionSecondEl=document.getElementById('commentSectionSecond')
+    function commentSectionSecond(){
+        commentSectionSecondEl.classList.toggle('comment_section_display')
+    }
+    let commentSectionThirdEl=document.getElementById('commentSectionThird')
+    function commentSectionThird(){
+        commentSectionThirdEl.classList.toggle('comment_section_display')
+    }
+    let commentSectionFourthEl=document.getElementById('commentSectionFourth')
+    function commentSectionFourth(){
+        commentSectionFourthEl.classList.toggle('comment_section_display')
+    }
